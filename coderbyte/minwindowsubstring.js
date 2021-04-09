19ae248fef29b62fb9f241162f86827b8f004639
tom-cheung <wai.tomcheung@gmail.com>
2021-04-08 23:51:12 -0700

    revise minwindowsubstring solution

--- coderbyte/minwindowsubstring.js
+++ coderbyte/minwindowsubstring.js
@@ -57,6 +57,51 @@ function MinWindowSubstring(strArr) {
     return true; 
   }
 
-  console.log(containsAll('affhkkse', 'fhea'))
+  // console.log(containsAll('affhkkse', 'fhea'))
 
 //   console.log(MinWindowSubstring(["a affh kksemckelloe", "fhea"]))
+
+// aaabaaddae / aed 
+// a a a b a a d dae
+
+// abc  
+
+function MinWindowSubstring2(strArr) {
+  let target = strArr[1];
+  let search = strArr[0];
+
+  // creates the hash of key value pairs for the target 
+  let targetList = {};
+  let searchList = {}; 
+
+  for(let char of target) {
+    if(targetList[char] === undefined) {
+      targetList[char] = 1; 
+    } else {
+      targetList[char]++; 
+    }
+  }
+
+  let left = 0; 
+  let right = 0; 
+
+  while(right < search.length) {
+    let subS = search.slice(left, right + 1);
+    
+    for(let char of subS) {
+      if(targetList[char] !== undefined && searchList[char] === undefined) {
+        searchList[char] = 1; 
+      } else if(targetList[char] !== undefined && searchList[char] !== undefined) {
+        searchList[char]++; 
+      }
+    }
+    // console.log(subS);
+    right++;; 
+  }
+
+  console.log(searchList)
+
+  return targetList; 
+}
+
+console.log(MinWindowSubstring2(["aaffhkksemckelloe", "fhea"]))
