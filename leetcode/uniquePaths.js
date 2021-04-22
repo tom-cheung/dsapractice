2d07eddde089c3146506d5af30f9f74714ae2add
tom-cheung <wai.tomcheung@gmail.com>
2021-04-22 16:44:56 -0700

    implement iterative solution for uniquePaths

--- leetcode/uniquePaths.js
+++ leetcode/uniquePaths.js
@@ -27,3 +27,31 @@ var uniquePaths = function(m, n, memo={}) {
 // 1,1        1,2        1,3
 // 2,1        2,2        2,3 => why is this 2 instead of 3? 
 // 3,1        3,2        3,3
+
+
+// iterative solution
+
+//     let grid = new Array(m).fill(new Array(n).fill(1)); 
+
+function iterativeUniquePaths() {
+    
+    let lastValue = 1; 
+    
+    for(let i = 0; i < grid.length; i++) {
+        for(let y = 0; y < grid[i].length; y++) {
+            
+            if(i !== 0 && y !== 0) {
+                
+                let topValue = grid[i - 1][y]; 
+                let leftValue = grid[i][y - 1]; 
+                
+                let sum = topValue + leftValue; 
+                grid[i][y] = sum; 
+                lastValue = sum; 
+                
+            }
+        }
+    }
+    
+    return lastValue; 
+}
