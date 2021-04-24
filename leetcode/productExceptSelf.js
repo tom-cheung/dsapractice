d4815e253c586dba5bee55c2ae2ca7c68a62134d
tom-cheung <wai.tomcheung@gmail.com>
2021-04-23 21:32:37 -0700

    implement optimal solution for productExceptSelf

--- leetcode/productExceptSelf.js
+++ leetcode/productExceptSelf.js
@@ -29,3 +29,42 @@ var productExceptSelf = function(nums) {
     return retArr; 
     
 };
+
+// time complexity wise this is O(n) but it is faster than the above which is also O(n)
+
+// it's easier to understand if instead of mutating prod array you generate two arrays, one for left products and one for right 
+// looks like 
+
+// [1, 2, 3, 4]
+// left => [1, 1, 2, 6]
+// right => [24, 12, 4, 1]
+// notice that each number is the product of all the numbers to the indicated side 
+// what is missing is itself and all the numbers to the opposite side 
+// so if you were to get both sides, multiplying them together would give you the product of all numbers aside from itself 
+
+// using the example [1, 2, 3, 4]
+// notice on right [24, 12, 4, 1], the 12 is the product of 3 * 4 but does not include 2 or 1
+// notice on left [1, 1, 2, 6], the 1 is the product of 1, and does not include 2, 3, 4 
+// so using both arrays you can get the products of either side of the number 
+
+
+
+var productExceptSelf2 = function(nums) {
+    
+    let prodArr = new Array(nums.length).fill(1); 
+    let prodLeft = 1; 
+    let prodRight = 1; 
+    
+    for(let i = 0; i < nums.length; i++) {
+        prodArr[i] = prodLeft; 
+        prodLeft *= nums[i];
+    }
+    
+    for(let i = nums.length - 1; i >= 0; i--) {
+        prodArr[i] *= prodRight; 
+        prodRight *= nums[i]
+    }
+    
+    return prodArr; 
+    
+};
