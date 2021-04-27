0aa56ac23b33f76f2866e9dee08f7cbb5cecdb61
tom-cheung <wai.tomcheung@gmail.com>
2021-04-26 17:37:31 -0700

    implement optimal solution for moveZero

--- leetcode/moveZeroes.js
+++ leetcode/moveZeroes.js
@@ -1,7 +1,3 @@
-
-
-
-
 // bubble sort 
 
 function moveZero(nums) {
@@ -25,3 +21,35 @@ function moveZero(nums) {
 }
 
 // [1, 3, 12, 0, 0]
+
+[1, 0, 43, 0, 2, 1]
+
+[1, 43, 2, 1, 0, 0]
+
+[0, 1, 0, 3, 12]; 
+
+// take out 0 
+// place at the end 
+// 0 gets pushed to the end 
+
+
+
+// 2n => n 
+// space is n 
+
+function moveZero(nums) {
+
+    let nonZero = []; // time => O(1); 
+
+    for(let i = 0; i < nums.length; i++) { // time => O(n)
+        if(num !== 0) nonZero.push(nums[i]); 
+        nums[i] = 0; 
+    }
+
+    for(let i = 0; i < nonZero.length; i++) { // time => O(n) 
+        nums[i] = nonZero[i];
+    }
+
+    return nums; 
+
+}
