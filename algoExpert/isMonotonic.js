1568e45a374525fa9eee5308a41cc2895e7204d2
tom-cheung <wai.tomcheung@gmail.com>
2021-04-28 00:10:27 -0700

    time and space complexity analysis for isMonotonic

--- algoExpert/isMonotonic.js
+++ algoExpert/isMonotonic.js
@@ -1,10 +1,10 @@
 function isMonotonic(array) {
     // Write your code here.
-      if(array.length <= 1) return true; 
-      let directions = {}; 
+    //   if(array.length <= 1) return true; // O(n) time  dont need this, since if directions is empty it returns true as well 
+      let directions = {}; // O(n) time 
       // console.log(direction)
   
-      for(let i = 0; i <= array.length - 2; i++) {
+      for(let i = 0; i <= array.length - 2; i++) { // O(n)
           let num1 = array[i]; 
           let num2 = array[i+1]; 
           
@@ -12,5 +12,5 @@ function isMonotonic(array) {
           if(num1 < num2) directions['increasing'] = true; 
       }
   
-      return Object.keys(directions).length <= 1; 
+      return Object.keys(directions).length <= 1; // max keys 2, constant? 
   }
