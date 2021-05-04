7308a421c365f295f4eed78a34f46ea2c3460234
tom-cheung <wai.tomcheung@gmail.com>
2021-05-03 20:28:55 -1000

    add notes to solution

--- leetcode/flattenBinaryTree.js
+++ leetcode/flattenBinaryTree.js
@@ -1,8 +1,12 @@
+
 var flatten = function(root) {
     let sortedNodes = DFS(root, []); 
     
     let currentNode = root; 
     
+    // iterate through the sorted array of nodes 
+    // set the current node to point to a new node
+    // reset the current node to the next node 
     for(let i = 1; i < sortedNodes.length; i++) {
         currentNode.right = sortedNodes[i];
         currentNode.left = null;
@@ -12,6 +16,7 @@ var flatten = function(root) {
     return root; 
 };
 
+// DFS to traverse the tree from left to right, obtaining the values in order 
 function DFS(node, sorted) {
     if(node === null) return sorted; 
     
@@ -22,3 +27,5 @@ function DFS(node, sorted) {
     
     return sorted; 
 }
+
+// time complexity O(2n) => O(n) space and time 
