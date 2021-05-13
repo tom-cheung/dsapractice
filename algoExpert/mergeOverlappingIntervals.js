9b7dec164ce13bd92cca349c08fd92925c61758a
tom-cheung <wai.tomcheung@gmail.com>
2021-05-12 20:15:42 -1000

    implement solution for mergeOverlappingIntervals

---
+++ algoExpert/mergeOverlappingIntervals.js
@@ -0,0 +1,33 @@
+function mergeOverlappingIntervals(arr) {
+    arr.sort((a, b) => a[0] - b[0] ); 
+      let merge = []; 
+      let start = 0; 
+      let end = 0; 
+  
+      for(let i = 0; i < arr.length; i++) {
+  
+          let currInterval = arr[i]; 
+          let currStart = currInterval[0]; 
+          let currEnd = currInterval[1]
+  
+          if(start === 0 && end === 0) {
+              start = currStart; 
+              end = currEnd; 
+          } 
+  
+          if(currStart <= end) {
+              if(currEnd > end) end = currEnd; 
+          } else {
+              merge.push([start, end]); 
+              start = currStart; 
+              end = currEnd; 
+          }	
+          
+          if(i === arr.length - 1) {
+              
+              merge.push([start, end]); 
+          }
+      }
+  
+      return merge; 
+  }
