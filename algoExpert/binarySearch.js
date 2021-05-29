f3b3a76b4bb91871cb18514cd0c70fe6e6e074e2
tom-cheung <wai.tomcheung@gmail.com>
2021-05-28 23:06:35 -0700

    implement optimal recursive solution

--- algoExpert/binarySearch.js
+++ algoExpert/binarySearch.js
@@ -1,5 +1,6 @@
 function binarySearch(arr, target, mid=0) {
       
+    // time O(1)
       if(arr.length === 1) {
           if(arr[0] === target) {
               return 0; 
@@ -8,12 +9,14 @@ function binarySearch(arr, target, mid=0) {
           }
       }
       
+    //   time O(1)
       mid = Math.floor(arr.length / 2); 
       let midEl = arr[mid];
       
       if(midEl === target) return mid; 
       
       if(midEl > target) {
+    //   slice has an O(n) time complexity, will affect the complexity of this algorithm
           let left = arr.slice(0, mid);
           return binarySearch(left, target, mid); 
       } {
@@ -29,3 +32,21 @@ function binarySearch(arr, target, mid=0) {
       
       
   }
+
+
+// O(log n) since I am not using slice 
+function binarySearch(arr, target, left=0, right=arr.length - 1) {
+
+    if(left > right) return -1; 
+
+    let mid = Math.floor((left + right) / 2); 
+
+    if(arr[mid] === target) return mid; 
+
+    if(target < arr[mid]) {
+        return binarySearch(arr, target, left, mid - 1);
+    } else {
+        return binarySearch(arr, target, mid + 1, right); 
+    }
+
+}
