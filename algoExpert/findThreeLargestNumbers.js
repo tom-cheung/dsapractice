21419ca52159c0c37a16305280593077c1cd0efc
tom-cheung <wai.tomcheung@gmail.com>
2021-06-02 17:37:43 -0700

    optimize solution for find three largest numbers

--- algoExpert/findThreeLargestNumbers.js
+++ algoExpert/findThreeLargestNumbers.js
@@ -25,3 +25,34 @@ function findThreeLargestNumbers(arr) {
 
 	return sorted; 
 }
+
+
+function findThreeLargestNumbers2(arr) {
+
+    const largest = [null, null, null]; 
+
+    for(const num of arr) {
+
+        if(largest[2] === null || largest[2] <= num) {
+            updateValue(largest, num, 2)
+        } else if(largest[1] === null || largest[1] <= num) {
+            updateValue(largest, num, 1)
+        } else if(largest[0] === null || largest[0] <= num) {
+            updateValue(largest, num, 0)
+        }
+    }
+
+    return largest; 
+}
+
+function updateValue(arr, num, idx) {
+    for(let i = 0; i <= idx; i++) {
+        if(i === idx) {
+            arr[i] = num; 
+        } else {
+            arr[i] = arr[i + 1]
+        }
+    }
+}
+
+console.log(findThreeLargestNumbers2([5,1,3,87,2,55,9320])); 
