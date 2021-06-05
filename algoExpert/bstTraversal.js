dbfe29a32aa14bf706f9e64bcac04e08785fc7ee
tom-cheung <wai.tomcheung@gmail.com>
2021-06-04 23:42:16 -0700

    implement solution for bstTraversal

---
+++ algoExpert/bstTraversal.js
@@ -0,0 +1,35 @@
+function inOrderTraverse(tree, array) {
+    // Write your code here.
+      
+      if(tree === null) return; 
+      inOrderTraverse(tree.left, array);
+      array.push(tree.value);
+      inOrderTraverse(tree.right, array); 
+      
+      return array 
+  }
+  
+  function preOrderTraverse(tree, array=[]) {
+    // Write your code here.
+      
+      if(tree === null) return;
+      
+      array.push(tree.value);
+      
+      preOrderTraverse(tree.left, array); 
+      preOrderTraverse(tree.right, array);
+      
+      return array;
+  }
+  
+  function postOrderTraverse(tree, array) {
+    // Write your code here.
+      
+      if(tree === null) return; 
+      
+      postOrderTraverse(tree.left, array);
+      postOrderTraverse(tree.right, array);
+      array.push(tree.value); 
+      
+      return array; 
+  }
--- algoExpert/validateBst.js
+++ algoExpert/validateBst.js
@@ -1,8 +1,10 @@
 function validateBst(node, min=-Infinity, max=Infinity) {
-    
-      if(node === null) return true; 
+
+      if(node === null) return true; // reached the end of the tree  
   
-      if(node.value < min || node.value >= max) return false; 
+      // for a bst to be valid each node must be within a min and max value
+      // as you traverse the tree, in this case recursively, you can track what those min an max values are 
+      if(node.value < min || node.value >= max) return false;
       
       let left = validateBst(node.left, min, node.value); 
       let right = validateBst(node.right, node.value, max); 
