97a99ce9d6395bd9d1e6fbc8f04ec975ab55a08d
tom-cheung <wai.tomcheung@gmail.com>
2021-06-06 23:56:14 -0700

    time and space complexity analysis for bubbleSort

--- algoExpert/bubbleSort.js
+++ algoExpert/bubbleSort.js
@@ -2,6 +2,11 @@ function bubbleSort(array) {
       
       let sorted = false; 
       
+      // time complexity O(n * n) or O(n^2) because worst case a number at the very end of the array needs to be shifted across the length of the array 
+      // which requires a number of passes equal to the length of the array 
+      // including one last time to confirm the array is sorted 
+
+      // space complexity is constant as I am overwriting the existing array, but would be O(n) if I had to create a copy of the array 
       while(!sorted) {
           sorted = true; 
           for(let i = 0; i < array.length - 1; i++) {
