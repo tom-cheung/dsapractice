34299c9f12ec3e5048373d9b16a7f873e7b2ec56
tom-cheung <wai.tomcheung@gmail.com>
2021-07-07 16:45:29 -0700

    add optimal solution for findKthLargestValueInBst

--- algoExpert/findKthLargestValueInBst.js
+++ algoExpert/findKthLargestValueInBst.js
@@ -1,9 +1,10 @@
 function findKthLargestValueInBst(tree, k) {
     // Write your code here.
-      let sortedVal = sortValue(tree)
+    let sortedVal = sortValue(tree)
     return sortedVal[sortedVal.length - k];
   }
-  
+
+
 function sortValue(tree) {
     if(tree === null) return; 
     
@@ -11,7 +12,30 @@ function sortValue(tree) {
     let right = sortValue(tree.right);
     
     let currVal = [tree.value];
+    // concat is an O(n) operation, which can slow down this solution 
     if(left) currVal = left.concat(currVal);
     if(right) currVal = currVal.concat(right); 
     return currVal; 
+}
+
+// more optimal solution 
+function findKthLargestValueInBst(tree, k) {
+    // Write your code here.
+
+    let sortedVal = [tree.value];
+    
+    sortValue(tree, sortedVal)
+    return sortedVal[sortedVal.length - k];
+  }
+
+
+function sortValue(tree, sortedVal) {
+    if(tree === null) return; 
+
+    // keep recursively traversing the tree until it reaches the left most (smallest value)
+    sortValue(tree.left, sortedVal); 
+    // push that value  
+    sortedVal.push(tree.value);
+    // recusively traverse the right side of the tree to capture the larger values.  
+    sortValue(tree.right, sortedVal); 
 }
